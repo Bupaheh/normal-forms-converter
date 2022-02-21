193f5ec5e35077b40df056104972099af1473b01
Bupaheh <pavel.balai@gmail.com>
2022-02-22 01:05:51 +0300

    Add instance Read Expr

--- src/Expr.hs
+++ src/Expr.hs
@@ -33,3 +33,69 @@ myShowsExprHelper :: Int -> String -> Int -> Expr -> Expr -> ShowS
 myShowsExprHelper opPriority s n a b | n <= opPriority = withoutBrackets
                                      | otherwise = myShows "(" . withoutBrackets . myShows ")"
     where withoutBrackets = myShowsExpr opPriority a . myShows s . myShowsExpr opPriority b
+
+    
+-- instance Read
+
+whitespaceRm s = pref ++ suff
+    where (pref, suff) = head $ lex s
+          
+data BinOp = Null | Eq | Impl | Or | And
+    deriving (Eq, Show, Ord)
+          
+getOp :: String -> (BinOp, String)
+getOp s = if (res == Null) then (res, s) else (res, whitespaceRm suff2)
+    where (operation, suff2) = head $ lex s
+          res = case operation of 
+                     "<->" -> Eq
+                     "->" -> Impl
+                     "+" -> Or
+                     "*" -> And
+                     _ -> Null
+  
+toConstr :: BinOp -> Expr -> Expr -> Expr
+toConstr op = case op of
+                   Eq -> (:<->)
+                   Impl -> (:->)
+                   Or -> (:+)
+                   And -> (:*)
+  
+applyOp :: [(Expr, BinOp)] -> Expr -> BinOp -> [(Expr, BinOp)]
+applyOp st@((ex1, op) : tl) ex2 context | context <= op = applyOp tl (toConstr op ex1 ex2) context
+                                        | otherwise = (ex2, context) : st
+                                        
+applyOp [] ex context = [(ex, context)]
+    
+instance Read Expr where
+  readsPrec _ = myReadExpr []          
+          
+myReadExpr :: [(Expr, BinOp)] -> ReadS Expr
+myReadExpr [(ex, Null)] s = [(ex, s)]
+
+myReadExpr st ('(' : s) = myReadExpr st' suff''
+    where (ex, ')' : suff) = head $ myReadExpr [] s
+          suff' = whitespaceRm suff
+          (op, suff'') = getOp suff'
+          st' = applyOp st ex op
+          
+myReadExpr st str@('!' : s) = case next of
+                               '(' : _ -> myReadExpr (applyOp st (constr ex1) op) suff'
+                               _ -> myReadExpr (applyOp st ex2 op2) suff2'
+    where (constr, next) = getNotConstr str
+          (ex1, ')' : suff) = head $ myReadExpr [] $ tail next
+          (op, suff') = getOp suff
+          (name, suff2) = head $ lex next
+          ex2 = constr $ Var name
+          (op2, suff2') = getOp suff2
+          
+myReadExpr st s = myReadExpr (applyOp st ex op) suff''
+    where (name, suff) = head $ lex s
+          suff' = whitespaceRm suff
+          ex = Var name
+          (op, suff'') = getOp suff' 
+          
+getNotConstr :: String -> (Expr -> Expr, String)
+getNotConstr ('!' : s) = (Not . f, str)
+    where (f, str) = getNotConstr $ whitespaceRm s
+    
+getNotConstr s = (id, s)
--- src/Lib.hs
+++ src/Lib.hs
@@ -4,5 +4,17 @@ module Lib
 
 import Expr(Expr(..))
 
-someFunc = putStrLn "fdf"
+t1 = read "(! !(a + !b) * c) -> ((a + c) * !d)" :: Expr
+
+toBasis :: Expr -> Expr
+toBasis (Var a) = Var a
+toBasis (Not a) = Not $ toBasis a
+toBasis (a :* b) = toBasis a :* (toBasis b)
+toBasis (a :+ b) = (toBasis a) :+ (toBasis b)
+toBasis (a :-> b) = (Not $ toBasis a) :+ (toBasis b)
+toBasis (a :<-> b) = (Not aa :+ bb) :* (Not bb :+ aa)
+    where aa = toBasis a
+          bb = toBasis b
+
+someFunc = putStrLn $ show t1
 
