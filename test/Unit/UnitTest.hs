036a51f4ed3d0aeedcea38e23bcc21ff9ea102bb
Bupaheh <pavel.balai@gmail.com>
2022-02-28 20:49:23 +0300

    Add toDNF unit tests

--- test/Unit/UnitTest.hs
+++ test/Unit/UnitTest.hs
@@ -7,7 +7,7 @@ import Test.Tasty.HUnit
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "Tests" [readShowTests, toNNFTests] 
+tests = testGroup "Tests" [readShowTests, toNNFTests, toDNFTests] 
 
 showRead :: String -> String
 showRead s = show (read s :: Expr)
@@ -29,7 +29,7 @@ readShowTests = testGroup "Read and show tests"
   
 var = read "a" :: Expr
 expr = read "a <=> ~(a & b => c | d)" :: Expr
-bigExpr = read "(~(a2 => a2 => a4) | (a6 | a5 => (a6 <=> a6)) => (~((a4 <=> a3) | a4) => ~(a3 => (a5 => a3)))) | ((a1 <=> a5) & a1 & a1 <=> ~a2 & a3 & a4 & ((a5 => a5) | a2 & a1))" :: Expr
+bigExpr = read "(~(a2 => a2 => a4) | (a6 | a5 => (a6 <=> a6)) => (~~((a4 <=> a3) | a4) => ~(a3 => (a5 => a3)))) | ((a1 <=> a5) & a1 & a1 <=> ~a2 & a3 & ~~a4 & ((a5 => a5) | a2 & a1))" :: Expr
 
 testNNF :: Expr -> Bool
 testNNF expr = isNNF nnf && isEq nnf expr
@@ -42,4 +42,17 @@ toNNFTests = testGroup "toNNF tests"
       assertBool "" $ testNNF expr
   , testCase "Big expression test" $
       assertBool "" $ testNNF bigExpr
+  ]
+  
+testDNF :: Expr -> Bool
+testDNF expr = isDNF dnf && isEq dnf expr
+  where dnf = toDNF expr
+
+toDNFTests = testGroup "toDNF tests"
+  [ testCase "Variable test" $ 
+      assertBool "" $ testDNF var
+  , testCase "Basic expression test" $
+      assertBool "" $ testDNF expr
+  , testCase "Big expression test" $
+      assertBool "" $ testDNF bigExpr
   ]
